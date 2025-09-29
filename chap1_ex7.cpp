ec58457c13770a3877a9253b8b8bcd37367c8d8b
Minh Nguyen <Minh.NP2417163@sis.hust.edu.vn>
2025-09-29 14:14:17 +0700

    Two pointer on unsorted sequence

---
+++ chap1_ex7.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, Q;
+    cin >> n >> Q;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    int M = 0, i = 0, j = n - 1;
+    while (i <= j)
+    {
+        if ((a[i] + a[j]) == Q)
+        {
+            M++;
+            i++;
+            j--;
+        }
+        else if ((a[i] + a[j]) < Q)
+        {
+            i++;
+        }
+        else j--;
+    }
+    cout << M << endl;
+    return 0;
+}
