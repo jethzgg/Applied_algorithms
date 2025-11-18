3c71d7777f7139da724991c2073c8987cfd66609
Minh Nguyen <Minh.NP2417163@sis.hust.edu.vn>
2025-11-18 15:29:12 +0700

    Update Max_even_subsequence.cpp

--- Slide/Session 4/Max_even_subsequence.cpp
+++ Slide/Session 4/Max_even_subsequence.cpp
@@ -23,12 +23,14 @@ int main()
     }
 
     f[0][0] = 0;
-    f[0][1] = 0;
+    f[0][1] = INT_MAX;
 
     for (int i = 1; i <= n; i++)
     {
         max_even_sum = max(max_even_sum, prefix_sum[i] - f[i - 1][prefix_sum[i] % 2]);
-        f[i][prefix_sum[i] % 2] = min(prefix_sum[i - 1], f[i][prefix_sum[i] % 2]);
+        f[i][0] = f[i - 1][0];
+        f[i][1] = f[i - 1][1];
+        f[i][prefix_sum[i] % 2] = min(prefix_sum[i], f[i - 1][prefix_sum[i] % 2]);
     }
 
     cout << max_even_sum << "\n";
