149b581de46dbe7e563f0f73acb12d51d8c6ec62
Minh Nguyen <Minh.NP2417163@sis.hust.edu.vn>
2025-11-18 13:32:18 +0700

    Create Max_even_subsequence.cpp

---
+++ Slide/Session 4/Max_even_subsequence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#define MAX 1000000
+
+using namespace std;
+
+int n;
+vector<int> a(MAX + 1);
+vector<int> prefix_sum(MAX + 1, 0);
+vector<vector<int>> f(MAX, vector<int>(2));
+int max_even_sum = INT_MIN;
+
+int main()
+{
+    cin >> n;
+    for (int i = 1 ; i <= n; i++)
+    {
+        cin >> a[i];
+        prefix_sum[i] = a[i];
+        prefix_sum[i] += prefix_sum[i - 1];
+    }
+
+    f[0][0] = 0;
+    f[0][1] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        max_even_sum = max(max_even_sum, prefix_sum[i] - f[i - 1][prefix_sum[i] % 2]);
+        f[i][prefix_sum[i] % 2] = min(prefix_sum[i - 1], f[i][prefix_sum[i] % 2]);
+    }
+
+    cout << max_even_sum << "\n";
+
+    return 0;
+}
