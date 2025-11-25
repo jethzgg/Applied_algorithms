7ba241ed136f3366a53bff06a20ce7de8ae2bb0a
Minh Nguyen <Minh.NP2417163@sis.hust.edu.vn>
2025-11-25 15:37:45 +0700

    Update taxi.cpp

--- taxi.cpp
+++ taxi.cpp
@@ -12,31 +12,43 @@ vector<int> visited(MAX, 0);
 vector<int> path(MAX, 0);
 vector<vector<int>> c(MAX, vector<int>(MAX, 0));
 
+int check(int i, int k)
+{
+    if (visited[i]) return 0;
+    if (i > n)
+    {
+        if (visited[i - n] == 0) return 0;
+    }
+    else
+    {
+        if ((k - 1) > 0 && path[k - 1] <= n) return 0;
+    }
+    return 1;
+}
+
 void backtrack(int k)
 {
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= 2 * n; i++)
     {
-        if (visited[i] == 0)
+        if (check(i, k))
         {
+            path[k] = i;
             visited[i] = 1;
             cost += c[path[k - 1]][i];
-            cost += c[i][i + n];
-            path[k] = i;
-            if (k == n)
+            if (k == 2 * n)
             {
-                if (MIN > (cost + c[path[k]][0]))
+                if (MIN > (cost + c[i][0]))
                 {
-                    MIN = cost + c[path[k]][0];
+                    MIN = cost + c[i][0];
                 }
             }
             else
             {
-                int g = cost + cmin * (2 * (n - k) + 1);
+                int g = cost + cmin * (2 * n - k + 1);
                 if (g < MIN) backtrack(k + 1);
             }
             visited[i] = 0;
             cost -= c[path[k - 1]][i];
-            cost -= c[i][i + n];
         }
     }
 }
@@ -50,10 +62,10 @@ int main()
         for (int j = 0; j <= (2 * n); j++)
         {
             cin >> c[i][j];
-            if (c[i][j] < cmin) cmin = c[i][j];
+            if (0 < c[i][j] && c[i][j] < cmin) cmin = c[i][j]; // must be <> 0
         }
     }
     backtrack(1);
-    cout << MIN;
+    cout << MIN << "\n";
     return 0;
 }
