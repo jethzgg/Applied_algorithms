87821fb151222df00f84831d9bc869c199c49fd4
Minh Nguyen <Minh.NP2417163@sis.hust.edu.vn>
2025-12-30 09:27:20 +0700

    Fix bridge and Add articulation

--- finalterm/chap6/find_articulation.cpp
+++ finalterm/chap6/find_articulation.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 #include <algorithm>
 
 using namespace std;
 
 int n, m;
 vector<vector<int>> adj;
-vector<pair<int, int>> bridges;
 vector<int> num;
 vector<int> low;
+vector<int> articulations;
+
 int current_num = 0;
 
 void dfs(int u, int parent)
@@ -17,6 +17,7 @@ void dfs(int u, int parent)
     current_num++;
     num[u] = current_num;
     low[u] = current_num;
+    int childs = 0;
     for (auto v : adj[u])
     {
         if (v == parent) continue;
@@ -24,11 +25,21 @@ void dfs(int u, int parent)
         {
             dfs(v, u);
             low[u] = min(low[u], low[v]);
-            if (low[v] > num[u]) bridges.push_back(make_pair(u, v));
+            childs++;
+            if (parent != 0 && low[v] >= num[u] && 
+                find(articulations.begin(), articulations.end(), u) == articulations.end())
+            {
+                articulations.push_back(u);
+            }
         }
         else
         {
             low[u] = min(low[u], num[v]);
+        }
+        if (parent == 0 && childs >= 2 && 
+        find(articulations.begin(), articulations.end(), u) == articulations.end())
+        {
+            articulations.push_back(u);
         }
     }
 }
@@ -39,17 +50,28 @@ int main()
     adj.resize(n + 1, vector<int>());
     num.resize(n + 1, 0);
     low.resize(n + 1, 0);
+
     for (int i = 1; i <= m; i++)
     {
-        int u, v;
+        int u, v; 
         cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+
     dfs(1, 0);
-    for (auto i : bridges)
+
+    sort(articulations.begin(), articulations.end());
+
+    if (articulations.empty()) cout << -1 << "\n";
+    else
     {
-        cout << i.first << " " << i.second << "\n";
+        for (auto i : articulations)
+        {
+            cout << i << " ";
+        }
+
+        cout << "\n";
     }
     return 0;
 }
