a15d40b7dc4400e9e16fa8f7fc0644158d88efab
Minh Nguyen <Minh.NP2417163@sis.hust.edu.vn>
2026-01-24 17:31:52 +0700

    Create count_solutions.cpp

---
+++ finalterm/chap2/count_solutions.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, M;
+vector<int> a;
+vector<int> prefix_sum;
+vector<int> solution;
+int sum = 0, cnt = 0;
+
+void solve(int x)
+{
+    if (solution[x] == 0)
+    {
+        for (int i = 1; i <= (M / a[x]); i++)
+        {
+            solution[x] = i;
+            sum += (i * a[x]);
+            if (x == n)
+            {
+                if (sum == M)
+                {
+                    cnt++;
+                }
+            }
+            else
+            {
+                int g = sum + prefix_sum[n] - prefix_sum[x];
+                if (g <= M) solve(x + 1);
+            }
+            sum -= (i * a[x]);
+            solution[x] = 0;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> M;
+    a.resize(n + 1, 0);
+    prefix_sum.resize(n + 1, 0);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        prefix_sum[i] = prefix_sum[i - 1] + a[i];
+    }
+    solution.resize(n + 1, 0);
+
+    solve(1);
+    cout << cnt << "\n";
+    return 0;
+}
