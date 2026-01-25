0ff4e4760410d74383e1f7d6cdc7aefc9e0b937c
Minh Nguyen <Minh.NP2417163@sis.hust.edu.vn>
2026-01-26 00:08:53 +0700

    Create Warehouse.cpp

---
+++ finalterm/chap4/Warehouse.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N, T, D;
+    vector<int> a, t;
+    vector<vector<int>> dp;
+    cin >> N >> T >> D;
+    a.resize(N + 1, 0);
+    t.resize(N + 1, 0);
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> t[i];
+    }
+
+    dp.resize(N + 1, vector<int>(T + 1, 0));
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= T; j++)
+        {
+            for (int d = 1; d <= D; d++)
+            {
+                if (i - d >= 0)
+                {
+                    if (j >= t[i])
+                    {
+                        dp[i][j] = max(dp[i][j], dp[i - d][j - t[i]] + a[i]);
+                    }
+                }
+            }
+        }
+    }
+    int MAX = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= T; j++)
+        {
+            MAX = max(MAX, dp[i][j]);
+        }
+    }
+    cout << MAX << "\n";
+    return 0;
+}
